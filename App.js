f4f6a1dac2975457f480a504a63fa8566cc8a846
JM Fantin <joao.fantin@acad.pucrs.br>
2020-07-02 16:37:08 -0300

    Modifying State to show/hide TextInput

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {
   TextInput,
 } from "react-native";
 
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Feather } from "@expo/vector-icons";
 
 import BookCount from "./components/BookCount";
 
@@ -16,6 +16,7 @@ export default function App() {
   const [totalCount, setTotalCount] = useState(0);
   const [readingCount, setReadingCount] = useState(0);
   const [readCount, setReadCount] = useState(0);
+  const [addingNewBook, setAddingNewBook] = useState(false);
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView />
@@ -33,43 +34,53 @@ export default function App() {
         <Text style={{ fontSize: 24 }}>Book Worm</Text>
       </View>
       <View style={{ flex: 1 }}>
-        <View style={{ height: 50, flexDirection: "row" }}>
-          <TextInput
-            style={{ flex: 1, backgroundColor: "#ececec", paddingLeft: 5 }}
-            placeholder="Enter book name"
-            placeholderTextColor="#999"
-          />
-          <TouchableOpacity>
-            <View
-              style={{
-                width: 50,
-                height: 50,
-                backgroundColor: "#adb",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Ionicons name="ios-checkmark" color="white" size={40} />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View
-              style={{
-                width: 50,
-                height: 50,
-                backgroundColor: "#f99",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Ionicons name="ios-close" color="white" size={40} />
-            </View>
-          </TouchableOpacity>
-        </View>
+        {addingNewBook && (
+          <View style={{ height: 50, flexDirection: "row" }}>
+            <TextInput
+              style={{ flex: 1, backgroundColor: "#ececec", paddingLeft: 5 }}
+              placeholder="Enter book name"
+              placeholderTextColor="#999"
+            />
+            <TouchableOpacity>
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: "#f99",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Feather name="delete" color="white" size={26} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: "#adb",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-checkmark" color="white" size={40} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+
         <TouchableOpacity
+          onPress={() => {
+            setAddingNewBook(!addingNewBook);
+          }}
           style={{ position: "absolute", bottom: 20, right: 20 }}
         >
-          <Ionicons name="ios-add-circle" color="#99f" size={60} />
+          <Ionicons
+            name={addingNewBook ? "ios-remove-circle" : "ios-add-circle"}
+            color="#99f"
+            size={60}
+          />
         </TouchableOpacity>
       </View>
       <View
