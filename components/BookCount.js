bd93b4a89329c10f27091d14de4cb2286e16e6a4
JM Fantin <joao.fantin@acad.pucrs.br>
2020-07-03 09:36:48 -0300

    22. Functional or Stateless Components

--- components/BookCount.js
+++ components/BookCount.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 
-export default function BookCount(props) {
-  const [totalCount, setTotalCount] = useState(0);
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 18 }}>{props.title}</Text>
-      <Text>{props.count}</Text>
-    </View>
-  );
-}
+const BookCount = ({ title, count }) => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text style={{ fontSize: 18 }}>{title}</Text>
+    <Text>{count}</Text>
+  </View>
+);
+
+export default BookCount;
